9a7d156e2626aea41839aff7cdc1d497d1be454a
NehaSingh9528 <singhneha7579@gmail.com>
2026-05-23 15:28:48 +0530

    Added new example

--- 13-ArrayMethods/Map-Filter-Reduce.js
+++ 13-ArrayMethods/Map-Filter-Reduce.js
@@ -18,3 +18,12 @@ let reduceValue = num.reduce((value1, value2)=>
     return value1+value2;
 })
 console.log(reduceValue); //Output: 15 because reduce() applies the provided function to each element of the array, reducing it to a single value. In this case, it sums up all the elements of the array. 1 + 2 = 3, 3 + 3 = 6, 6 + 4 = 10, 10 + 5 = 15.
+
+//Here is another example of reduce() to find the maximum value in an array:
+
+let maxValue = num.reduce((max, value)=>
+{
+    return (value > max) ? value : max;
+}, num[0]); 
+console.log(maxValue); //Output: 5 because reduce() iterates through the array and compares each value with the current maximum value, updating the maximum value when a larger value is found. In this case, it finds that 5 is the largest value in the array.
+
